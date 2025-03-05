bc9cdd37a80b3a24e63ec4bfed3b1655d68ba8d7
gaoliang <33929539+gaoliangs@users.noreply.github.com>
2025-03-05 19:31:26 +0800

    Add files via upload

--- treevalue.py
+++ treevalue.py
@@ -1,167 +1,28 @@
 import matlab
 import matlab.engine
-import re
-from operator import itemgetter
 import subprocess
-import os
 
 
-def findsplit(s):
-    result = []
-    stack = []
-    current_part = ""
-
-    for char in s:
-        if char == ',' and not stack:
-            result.append(current_part.strip())
-            current_part = ""
-        else:
-            current_part += char
-            if char == '(':
-                stack.append(char)
-            elif char == ')':
-                if stack:
-                    stack.pop()
-
-    result.append(current_part.strip())
-    return result
-
-
-
-
-def find_matching_parentheses(s):
-    stack = []
-    result = []
-
-    for i, char in enumerate(s):
-        if char == '(':
-            stack.append(i)
-        elif char == ')':
-            if stack:
-                result.append([stack.pop(), i])
-
-    return result
-
-
-
-def findunresolvedsplit(tree):
-	brloc = find_matching_parentheses(tree)
-	for br in brloc:
-		unsp = tree[br[0]:br[1]+1]
-		csp1 = tree[:br[0]]
-		csp2 = tree[br[1]+1:]
-		cn = unsp.count(',')
-		bn = unsp.count('(')
-		#print(unsp,cn,bn)
-		if cn-bn>0:
-			return([unsp,csp1,csp2])
-			break
-
-def isthesame(str1,str2):
-	b1 = find_matching_parentheses(str1)
-	b2 = find_matching_parentheses(str2)
-	clu1 = [sorted(str1[bb1[0]:bb1[1]+1].replace('(', '').replace(')', '').split(',')) for bb1 in b1]
-	clu2 = [sorted(str2[bb2[0]:bb2[1]+1].replace('(', '').replace(')', '').split(',')) for bb2 in b2]
-	return sorted(clu1) == sorted(clu2)
-
-
-
-def find_last_common_ancestor(bloc,pair1,pair2):
-	for start,end in sorted(bloc,key =itemgetter(1)):
-		if (start<=pair1[0]) & (start<=pair2[0]) & (end>=pair1[1]) & (end>=pair2[1]):
-			return [start,end]
-			break
-
-
-def cluster_level(s,pair):
-	return s[:pair[0]].count('(')-s[:pair[0]].count(')')
-#print(cluster_level(treetopo,[6, 18]))
-
-
-
-
-def spr_x(s):
-    #get all clusters in tree string
-    bracket = find_matching_parentheses(s)
-    clu = [s[i[0]:i[1]+1] for i in bracket]
-    taxon = s.replace("(", "").replace(")", "").split(',')
-    fullclu = clu+taxon
-    
-    for c in clu:
-        two_split = findsplit(c[1:-1])
-        if ('X' == two_split[0])|('X' == two_split[1]):
-            original_c = c
-            newc = [two_split[0] if two_split[1]== 'X' else two_split[1]]
-            fullclu.remove(newc[0])
-            fullclu.remove('X')
-            fullclu.remove(c)
-    #print(fullclu)
-
-    delete_x = s.replace(original_c,newc[0])
-    clu_delete_x = [i.replace(original_c,newc[0]) for i in fullclu]
-    #print(clu_delete_x)
-
-    new = [f'({i},X)' for i in clu_delete_x]
-    #print('sdfgsdgf',clu_delete_x,new)
-    #de_replaced = re.sub(r'\bta1\b', , delete_x)
-
-    sprtree = []
-    for i in range(len(new)):
-        if '(' in clu_delete_x[i]:
-            sprtree.append(delete_x.replace(clu_delete_x[i],new[i]))
-        else:
-            sprtree.append(re.sub(r'\b' + re.escape(clu_delete_x[i]) + r'\b',new[i], delete_x))
-        #print(sprtree)
-
-    return sprtree
-
-def remove_duplicates(trees):
-    unique_trees = []
-
-    for i, tree1 in enumerate(trees):
-        is_duplicate = False
-
-        for j, tree2 in enumerate(trees[i + 1:]):
-            if isthesame(tree1, tree2):
-                is_duplicate = True
-                break
-
-        if not is_duplicate:
-            unique_trees.append(tree1)
-
-    return unique_trees
-
-
-current_directory = os.getcwd()
-
-markerfile = input("marker file: ")
-#markerfile = 'newseq1.csv'
-newicktree = input("newick tree file: ")
-#newicktree = 'tjtree_newick.txt'
-option = input("use parameter c (yes/no): ").lower()
+markerfile = input("Please input the marker file: ")
+newicktree = input("Please input the tree file: ")
+usec = input("use parameter c (yes/no): ").lower()
 useq = input("use parameter q (yes/no): ").lower()
 
 
 alltreetopo = []
-input_file_path = os.path.join(current_directory, newicktree)
-with open(input_file_path, 'r') as mark:
-   line = mark.readlines()
-for i in line:
-    alltreetopo.append(i[:-1])
+with open(newicktree, 'r') as mark:
+    for line in mark:
+        alltreetopo.append(line.strip())
+#print(alltreetopo)
 
-
-eng = matlab.engine.start_matlab()
 
 for treetopo in alltreetopo:
-    print('topo:',treetopo)
+    print('tree:',treetopo)
     if useq =='yes':
-        subprocess.run(["python3", "newlambda_q.py", treetopo, markerfile, option])
+        subprocess.run(["python3", "newlambda_q.py", treetopo, markerfile, usec])
     else:
-        subprocess.run(["python3", "newlambda.py", treetopo, markerfile, option])
-    print('start_matlab')
-	#eng = matlab.engine.start_matlab()
+        subprocess.run(["python3", "newlambda_cpp.py", treetopo, markerfile, usec])
+    eng = matlab.engine.start_matlab()
     F0=eng.treescore(nargout =1)
-	#eng.exit()
+    eng.exit()
 
-
-eng.exit()
